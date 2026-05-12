b6070dcfc87a7768e8959ea4eeab7df24c0cfbb3
Joshua Garcia <gjoshua9791@gmail.com>
2026-05-12 00:40:13 -0700

    Created project and added base starter code and files to be worked on

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <section className="hero">
+        <p className="eyebrow">Family-Owned Restaurant</p>
+        <h1>Wana Iguana</h1>
+        <p className="heroText">Delicious food, great taste, and a menu made for everyone.</p>
+
+        <div className="heroButtons">
+          <a href="#menu" className="primaryButton">View Menu</a>
+          <a href="tel:6612979222" className="secondaryButton">Call Us</a>
+        </div>
+      </section>
+
+    </main>
+
+  );
+
+}
+
+export default App
